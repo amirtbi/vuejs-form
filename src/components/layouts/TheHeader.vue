74eb56dcd5503e719288d5630e7e77ef60ad4536
amirtbi <torabi46@gmail.com>
2021-08-15 21:16:15 +0430

    addign some css styles into the TheHeader component

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1 class="header-title">Contact Us</h1>
+        <p>Any questions or remarks, Just write us a message !</p>
+    </header>
+</template>
+
+
+<style scoped>
+    header{
+        display: flex;
+        flex-direction: column;
+        width:100vw;
+        background:var(--primaryColor);
+        justify-content: center;
+        align-items: center;
+        
+    }
+
+    h1,p{
+        padding: 1rem;
+        margin:0;
+        color:white;
+        font-family: inherit;
+    }
+    p{
+        filter: opacity(0.5);
+        padding:0px 0px 10px 0;
+
+    }
+</style>
