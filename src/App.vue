8ffa6da60fbf4891047d199ff55a3c286f4860ea
amirtbi <torabi46@gmail.com>
2021-08-15 19:06:30 +0430

    updating json files

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from "./components/layouts/FormResource/TheForm.vue";
+
+export default {
+  components: {
+    TheForm,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,300&display=swap");
+
+:root {
+  --primaryColor: rgb(84, 5, 175);
+  --secondaryColor: rgb(182, 181, 181);
+  --accentColor: pink;
+  --primaryBubbleColor: #f3adb9;
+  --secondaryBubbleColor: rgba(53, 74, 189, 0.7);
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
