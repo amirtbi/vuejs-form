140d409a5e6a565ea370a991b79a651d9c37f916
amirtbi <torabi46@gmail.com>
2021-08-16 17:29:05 +0430

    add some styles and modifications in rating control component

--- src/components/layouts/FormResource/RatingControl.vue
+++ src/components/layouts/FormResource/RatingControl.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="rating-container">
+        <p id="rating-description">Rate your experience</p>
         <ul>
             <li :class="{active:activeOption==='poor'}"><button @click="activateOption('poor')" type="button">Poor</button></li>
             <li :class="{active:activeOption==='medium'}"><button @click="activateOption('medium')" type="button">Medium</button></li>
@@ -29,17 +30,22 @@ export default {
 </script>
 <style scoped>
 .active{
-    border:1px solid purple;
+    background-color: blueviolet;
+    color: white;
 }
 div.rating-container{
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 1.2rem;
 }
 ul{
     display: flex;
     flex-direction: row;
     list-style: none;
     padding: 0;
+    margin: 0;
     
 
 }
@@ -50,18 +56,27 @@ li{
     align-items: center;
     margin: 0.2rem;
 }
+p#rating-description{
+    font-weight: bold;
+    font-family: inherit;
+    text-transform: uppercase;
+}
 button{
     
-    color: black;
+    color: white;
     border:none;
     outline: none;
-    padding: 0.2rem 1rem;
+    padding: 0.4rem 1rem;
     cursor:pointer;
-    font-weight: bold;
+    
+    font-family: "Karla", sans-serif;
+    background-color: var(--secondaryBubbleColor);
+    border-radius: 2px;
 
 }
 button:hover{
-    color: rgb(161, 0, 153);
+    color: white;
+    background-color: var(--primaryColor);
     
 }
 </style>
