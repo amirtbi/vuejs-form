94431034a5a4b48e213068b539bb1e77dbb2415b
amirtbi <torabi46@gmail.com>
2021-08-16 13:04:29 +0430

    defining some logics for checkbox and radiobuttons

--- src/components/layouts/FormResource/TheFormControl.vue
+++ src/components/layouts/FormResource/TheFormControl.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="contact-container__form">
-    <form class="form-control" @submit.prevent>
+    <!--form -->
+    <form class="form-control" @submit.prevent="showUserInput">
       <!--form-row-->
       <div class="form-row">
         <div class="form-row__field">
           <label for="firstname">First Name</label>
-          <input type="text" id="firstname" name="firstname" />
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            v-model.trim="userName"
+          />
         </div>
         <div class="form-row__field">
           <label for="lastname">Last Name</label>
@@ -19,69 +25,148 @@
         </div>
         <div class="form-row__field">
           <label for="firstname">Phone</label>
-          <input type="tell" id="phone" name="phone" />
+          <input type="tell" id="phone" name="phone" ref="userPhone" />
         </div>
       </div>
       <div class="form-row">
         <div class="form-row__field">
           <label for="age">Your Age</label>
-          <input type="number" id="age" name="age" min="10" max="60" />
+          <input
+            type="number"
+            id="age"
+            name="age"
+            min="10"
+            max="60"
+            v-model="userAge"
+          />
         </div>
         <div class="form-row__field">
           <label for="howHear">How Did you hear about us?</label>
-          <select name="howHear" id="howHear">
+          <select name="howHear" id="howHear" v-model="referre">
             <option value="Google">Google</option>
             <option value="Facebook">Facebook</option>
             <option value="Instagram">Instagram</option>
           </select>
         </div>
       </div>
+      <!--user interest checkboxes-->
       <div class="form-row form-row--justify">
         <div class="form-row--checkbox">
           <p>What are you intrested in?</p>
           <div class="form-checkbox">
-            <input type="checkbox" name="user-intrest" />
+            <input
+              type="checkbox"
+              name="user-intrest"
+              value="news"
+              v-model="interest"
+            />
             <label for="user-intrest">News</label>
           </div>
           <div class="form-checkbox">
-            <input type="checkbox" name="user-intrest" />
+            <input
+              type="checkbox"
+              name="user-intrest"
+              value="tutorial"
+              v-model="interest"
+            />
             <label for="user-intrest">Tutorials</label>
           </div>
           <div class="form-checkbox">
-            <input type="checkbox" name="user-intrest" />
+            <input
+              type="checkbox"
+              name="user-intrest"
+              value="nothing"
+              v-model="interest"
+            />
             <label for="user-intrest">Nothing</label>
           </div>
         </div>
+        <!--user how learn radiobuttons -->
         <div class="form-row--radiobox">
           <p>How Do You Learn?</p>
           <div class="form-radiobox">
-            <input type="radio" name="user-intrest" />
-            <label for="user-intrest">News</label>
+            <input
+              type="radio"
+              name="learning-how"
+              v-model="how"
+              value="Video course"
+            />
+            <label for="user-intrest">Video Courses</label>
           </div>
           <div class="form-radiobox">
-            <input type="radio" name="user-intrest" />
-            <label for="user-intrest">Tutorials</label>
+            <input
+              type="radio"
+              name="learning-how"
+              v-model="how"
+              value="Blogs"
+            />
+            <label for="user-intrest">Blogs</label>
           </div>
           <div class="form-radiobox">
-            <input type="radio" name="user-intrest" />
-            <label for="user-intrest">Nothing</label>
+            <input
+              type="radio"
+              name="learning-how"
+              v-model="how"
+              value="Other"
+            />
+            <label for="user-intrest">Other</label>
           </div>
         </div>
       </div>
+
       <div class="button-container">
-        <base-button @click="displayAlert" mode="action">SUBMIT</base-button>
-      </div>   
+        <div class="confirm-container">
+          <input type="checkbox" id="confirmed-terms" name="confirmed-terms" v-model="confirmed" />
+          <label for="confirmed-terms">Do you agree all terms?</label>
+        </div>
+        <base-button mode="action">SUBMIT</base-button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  methods:{
-    displayAlert(){
-      alert("form is submitted")
-          }
-  }
+  data() {
+    return {
+      userName: "",
+      userAge: null,
+      referre: "Google",
+      interest: [],
+      how: null,
+      confirmed:false,
+    };
+  },
+
+  methods: {
+    showUserInput() {
+      console.log("username:" + this.userName);
+      console.log("user Age :" + this.userAge);
+      //show type of this.$refs
+      console.log("user phone number :" + typeof this.$refs.userPhone.value);
+      this.userName = "";
+      console.log("referre :" + this.referre);
+      this.referre = "Google";
+      console.log("User interest :");
+      console.log("=======");
+      for (let intr of this.interest) {
+        console.log(intr);
+      }
+
+      console.log("=======");
+      //reseting the checkboxes
+      this.interest = [];
+
+      console.log("user learning approach:");
+      console.log(this.how);
+
+      //reseting radioboxes
+      this.how = null;
+      //show true/false checkbox 
+      console.log(this.confirmed);
+      this.confirmed = false;
+    },
+  },
 };
 </script>
 
@@ -103,9 +188,8 @@ div.form-row {
   justify-content: space-between;
   align-items: center;
 }
-div.form-row--justify{
-    justify-content: flex-start;
-
+div.form-row--justify {
+  justify-content: flex-start;
 }
 div.form-row__field {
   display: flex;
@@ -145,13 +229,12 @@ select {
   border: 2px solid rgba(180, 179, 179, 0.8);
   outline: none;
 }
-div.form-row--radiobox{
-    margin-left: 5rem;
+div.form-row--radiobox {
+  margin-left: 5rem;
 }
 div.form-row--checkbox,
 div.form-row--radiobox {
   flex-direction: column;
- 
 }
 div.form-row--checkbox p,
 div.form-row--radiobox p {
@@ -164,7 +247,7 @@ div.form-checkbox,
 div.form-radiobox {
   display: flex;
   flex-direction: row;
-  width: 20%;
+  width: 100%;
   justify-content: flex-start;
   align-content: center;
   padding: 0.2rem 1.2rem;
@@ -175,13 +258,24 @@ div.form-radiobox label {
   font-weight: bold;
   margin-left: 0.2rem;
 }
-div.button-container{
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    align-content: center;
-    width: 100%;
-    margin-top: 1rem;
-    padding: 1.2rem;
+div.button-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  width: 100%;
+  margin-top: 1rem;
+  padding: 1.2rem;
+}
+div.confirm-container{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+div.confirm-container label{
+  font-weight: bold;
+  font-family: inherit;
+  margin-left: 0.2rem;
 }
 </style>
